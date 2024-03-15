a3d60fb613d723c69ddef6a5c5ba967236d95110
uyamini <uyamini1996@gmail.com>
2024-03-15 16:51:34 -0500

    added html file

---
+++ app.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*//*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
