e023b97ecc8398c4e3d1f41bbe2148bb335d116a
uyamini <uyamini1996@gmail.com>
2024-03-18 11:26:46 -0500

    added createBoard function

--- app.js
+++ app.js
@@ -26,16 +26,42 @@ const startPieces = [
 /*----- event listeners -----*/
 
 
-/*----- functions -----*//*----- constants -----*/
-
-
-/*----- state variables -----*/
-
-
-/*----- cached elements  -----*/
-
-
-/*----- event listeners -----*/
-
-
 /*----- functions -----*/
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML = startPiece;
+        
+        square.setAttribute('square-id', i);
+        //square.classList.add('beige');
+        //setting the colors of the board
+        //finds the row number, i is the column number (got this formula from the internet)
+        const row = Math.floor((63-i)/8) + 1;
+        if(row % 2 === 0)
+        {
+            square.classList.add( i % 2 === 0 ?"beige" : "brown")
+        }
+        else{
+            square.classList.add( i % 2 === 0 ?"brown" : "beige")
+        }
+
+        //setting the colors of the pieces
+        if( i <= 15)
+        {
+            square.firstChild.firstChild.classList.add('black');
+        }
+
+        //last 16
+        if( i >= 48 )
+        {
+            square.firstChild.firstChild.classList.add('white');
+        }
+
+        gameBoard.append(square);
+    })
+
+}
+
+createBoard();
